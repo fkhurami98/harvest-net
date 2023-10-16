81ee7c2081514410d4e74d09f6b44d617710133e
Farhad Khurami <farhadkhurami@Farhads-MacBook-Pro.local>
2023-10-16 15:01:37 +0100

    Added mock implementation of master/worker scripts

---
+++ harvest_net/master_node.py
@@ -0,0 +1,76 @@
+import socket
+import threading
+from queue import Queue
+
+# Constants
+HOST: str = '0.0.0.0'
+PORT: int = 12345
+BUFFER_SIZE: int = 1024
+
+URLS: list = [
+    "http://example.com/news1",
+    "http://example.com/news2",
+    "http://example.com/news3",
+]
+
+# Instintaniate a task_queue using the Queue() class
+task_queue = Queue()
+
+# Populating the task queue
+for url in URLS:
+    task_queue.put(url)
+
+# Instintaniate a empty result list
+results = []
+
+
+def task_distributor():
+    """
+    This function listens for worker connections and distributes tasks to them.
+    """
+    distributor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    distributor.bind((HOST, PORT))
+    distributor.listen(5)
+    print("Task distributor started, waiting for worker connections...\n" + "-"*50)
+
+    while True:
+        worker, addr = distributor.accept()
+        print(f"\nConnected to worker @ {addr}")
+        if not task_queue.empty():
+            task = task_queue.get()
+            print(f"\nSending task: {task} to worker @ {addr}")
+            worker.send(task.encode())
+        else:
+            print("\nNo tasks available. Sending 'NO_TASK' message to worker.")
+            worker.send("NO_TASK".encode())
+        worker.close()
+
+
+def result_collector():
+    """
+    This function listens for results from workers and collects them.
+    """
+    collector = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    collector.bind((HOST, PORT + 1))
+    collector.listen(5)
+    print("\nResult collector started, waiting for results...\n" + "-"*50)
+
+    while True:
+        worker, addr = collector.accept()
+        data = worker.recv(BUFFER_SIZE).decode()
+        if data != "NO_TASK":
+            results.append(data)
+            print(f"\nReceived data from worker @ {addr}: {data}")
+        worker.close()
+
+
+# Starting the distributor and collector threads
+threading.Thread(target=task_distributor).start()
+threading.Thread(target=result_collector).start()
+
+# Keeping the script running for testing purposes
+try:
+    while True:
+        pass
+except KeyboardInterrupt: # Ctrl + C to see results.
+    print("\nResults collected:", results)
---
+++ harvest_net/worker_node.py
@@ -0,0 +1,42 @@
+import socket
+import time
+
+# Constants
+HOST: str = '127.0.0.1'
+PORT: int = 12345
+BUFFER_SIZE: int = 1024
+
+def worker_node():
+    """
+    This function defines the functionality of a worker node.
+    It requests a task, which it then processeses, then sends the result back to the master node.
+    """
+    print("\n" + "-"*50)
+    
+    # Requesting a task from the master node
+    worker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    worker.connect((HOST, PORT))
+    print("\nConnected to master node for task...")
+    task = worker.recv(BUFFER_SIZE).decode()
+    worker.close()
+
+    # If there's no task available, the worker remains idle
+    if task == "NO_TASK":
+        print("\nNo task available. Worker is idle.")
+        return
+
+    # Fake task
+    print(f"\nReceived task: {task}. Processing...")
+    time.sleep(2) # Simulating proccessing
+    scraped_data = f"Mock data from {task}"
+
+    # Sending processed result back to the master node
+    collector = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    collector.connect((HOST, PORT + 1))
+    print(f"\nSending mock data for {task} to master node...")
+    collector.send(scraped_data.encode())
+    collector.close()
+
+    print("-"*50 + "\n")
+
+worker_node()
